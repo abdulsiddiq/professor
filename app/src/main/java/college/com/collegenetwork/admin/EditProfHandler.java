4a6b8186f0a3177a8cacdc3748c7d0e221e9e91e
mohamed.sidique <IN\mohamed.sidique@ril.com>
2017-12-02 20:07:28 +0530

    added delete

--- app/src/main/java/college/com/collegenetwork/admin/EditProfHandler.java
+++ app/src/main/java/college/com/collegenetwork/admin/EditProfHandler.java
@@ -83,7 +83,7 @@ public class EditProfHandler implements View.OnClickListener, IWebResponseProces
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        builder.setNegativeButton("Delete", new DialogInterface.OnClickListener()
         {
             @Override
             public void onClick( DialogInterface dialog, int which )
